5218b5295cd1070a4b5eec3f0f5e0516cf2c66c2
Raghav Mathur <107276902+mathurraghav01@users.noreply.github.com>
2025-09-11 12:19:41 +0530

    Update index.ts

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,21 @@
-import express from "express";
+import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
 dotenv.config();
-const app = express();
+
+const app: Application = express();
+
 
 app.use(cors());
 app.use(express.json());
 
-// Simple route
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Backend is running 🚀");
 });
 
-// Use PORT from .env or default to 5000
-const PORT = process.env.PORT || 5001;
+const PORT: number = parseInt(process.env.PORT || "5001", 10);
+
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
